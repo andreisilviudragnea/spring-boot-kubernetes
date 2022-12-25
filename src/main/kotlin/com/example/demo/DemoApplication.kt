568792d298fa0c70d2451c4bc3b0d6ae8fa43fd8
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2022-12-25 12:23:06 +0200

    More details

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,6 +2,7 @@ package com.example.demo
 
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -25,7 +26,7 @@ fun main(args: Array<String>) {
 	// 4. METADATA request in producer thread (all topics)
 	// 5. INIT_PRODUCER_ID request in producer thread
 
-	Thread.sleep(10_000)
+	Thread.sleep(5_000)
 
 	println("partitionsForTopic calls")
 	println(producer.partitionsFor("my-topic"))
@@ -33,6 +34,14 @@ fun main(args: Array<String>) {
 	// 1. DNS resolution in producer thread (check if still happens in constructor call too)
 	// 2. API_VERSIONS request in producer thread
 	// 3. METADATA request in producer thread (specified topic)
+
+	Thread.sleep(5_000)
+
+	println("send calls")
+
+	producer.send(ProducerRecord("my-topic", "cevaaa"))
+
+	// 1. PRODUCE request in producer thread
 
 	Thread.sleep(3600_000)
 
