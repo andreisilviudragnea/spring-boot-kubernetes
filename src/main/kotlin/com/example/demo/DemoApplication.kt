cd956e3753992577c83aaf3c18b853377bd648a8
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2022-12-25 12:18:16 +0200

    More details

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -17,11 +17,24 @@ fun main(args: Array<String>) {
 		it[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "my-cluster-kafka-0.my-cluster-kafka-brokers.kafka.svc:9092"
 	})
 
-	Thread.sleep(5_000)
+	println("Constructor calls")
 
+	// 1. DNS resolution in constructor call thread
+	// 2. DNS resolution in producer thread
+	// 3. API_VERSIONS request in producer thread
+	// 4. METADATA request in producer thread (all topics)
+	// 5. INIT_PRODUCER_ID request in producer thread
+
+	Thread.sleep(10_000)
+
+	println("partitionsForTopic calls")
 	println(producer.partitionsFor("my-topic"))
 
-	Thread.sleep(60_000)
+	// 1. DNS resolution in producer thread (check if still happens in constructor call too)
+	// 2. API_VERSIONS request in producer thread
+	// 3. METADATA request in producer thread (specified topic)
+
+	Thread.sleep(3600_000)
 
 	runApplication<DemoApplication>(*args)
 }
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 ./gradlew build
-nerdctl --namespace k8s.io build -t demo:1.5 .
+nerdctl --namespace k8s.io build -t demo:1.1 .
 kubectl apply -f demo-deployment.yaml
 kubectl get pods
