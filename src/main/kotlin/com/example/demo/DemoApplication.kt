0a2448c0ade0f07445e754e72b2c97ccc10ab8be
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2023-01-02 15:19:26 +0200

    Activated Kafka metrics

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,23 +2,40 @@ package com.example.demo
 
 import io.micrometer.core.instrument.logging.LoggingMeterRegistry
 import org.apache.kafka.clients.producer.Producer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.actuate.autoconfigure.metrics.KafkaMetricsAutoConfiguration
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.kafka.core.KafkaTemplate
 
 @SpringBootApplication
 @AutoConfiguration(after = [KafkaMetricsAutoConfiguration::class])
 class DemoApplication {
+	companion object {
+		val logger: Logger = LoggerFactory.getLogger(DemoApplication::class.java)
+	}
+
 	@Bean
 	fun loggingMeterRegistry() = LoggingMeterRegistry()
 
-	@Bean
-	fun kafkaProducerBean(): Producer<String, String> = kafkaProducer()
+//	@Bean
+//	fun kafkaProducerBean(): Producer<String, String> = kafkaProducer()
 
 //	@Bean
 //	fun kafkaAdminClientBean(): Admin = kafkaAdminClient()
+
+	@Bean
+	fun beanClass(kafkaTemplate: KafkaTemplate<String, String>): BeanClass {
+		logger.info("UsingKafkaTemplate")
+		kafkaTemplate.partitionsFor("my-topic")
+		kafkaTemplate.send("my-topic", "my-template-message")
+		return BeanClass()
+	}
+
+	class BeanClass
 }
 
 fun main(args: Array<String>) {
